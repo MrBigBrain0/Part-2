638f55b1d4205674b635368f247cd81508073b11
MSI\silvi <sicilisi@sheridancollege.ca>
2024-02-07 10:06:04 -0600

    task1 completed; night will animate walk to mouse down, take damge and will die.

---
+++ Assets/Week 5/Scripts/Knight.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3f;
+    bool clickOnSelf = false;
+    public float health;
+    public float MaxHealth = 5;
+    bool isDead;
+
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+       animator = GetComponent<Animator>();
+       health = MaxHealth;
+        isDead = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDead) return;
+        movement = destination - (Vector2)transform.position;
+        if(movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+    void Update()
+    {
+        if (isDead) return;
+        if (Input.GetMouseButtonDown(0) && !clickOnSelf)
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("Movement", movement.magnitude);
+    }
+
+    private void OnMouseDown()
+    {
+        if (isDead) return;
+        clickOnSelf = true;
+        TakeDamage(1);
+    }
+
+    private void OnMouseUp()
+    {
+        clickOnSelf = false;
+    }
+
+    void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health,0,MaxHealth);
+        if(health <= 0)
+        {
+            isDead = true;
+            animator.SetTrigger("Death");
+        }
+        else
+        {
+            isDead = false;
+            animator.SetTrigger("TakeDMG");
+        }
+    }
+}
