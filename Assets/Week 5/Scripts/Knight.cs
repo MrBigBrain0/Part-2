71745b25ddcc308fe4f45bc6a42f1d8e04894ca7
MSI\silvi <sicilisi@sheridancollege.ca>
2024-02-14 09:24:32 -0600

    Knight Does not Interact with UI

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -37,7 +37,7 @@ public class Knight : MonoBehaviour
     void Update()
     {
         if (isDead) return;
-        if (Input.GetMouseButtonDown(0) && !clickOnSelf)
+        if (Input.GetMouseButtonDown(0) && !clickOnSelf && !EventSystem.current.IsPointerOverGameObject())
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
