3e4898428fccee6472bff7787803986914cf255e
MSI\silvi <sicilisi@sheridancollege.ca>
2024-02-14 18:42:15 -0600

    It works now. though I found a back words way(I Think) of doing it were I did stuff in HealthBar. Task-3 complete

--- Assets/Week 5/Scripts/HealthBar.cs
+++ Assets/Week 5/Scripts/HealthBar.cs
@@ -7,8 +7,20 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("HealthBar", slider.value);
+    }
     public void TakeDamage(float Damage)
-    { 
+    {
         slider.value -= Damage;
+
+        HealthBarNew();
+
+    }
+
+    public void HealthBarNew()
+    {
+        PlayerPrefs.SetFloat("HealthBar", slider.value);
     }
 }
